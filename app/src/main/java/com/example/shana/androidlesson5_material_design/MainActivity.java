4247aa6e0bcd6e9658cf631dcd7eef7caa50eac7
Twilight <rie00002@gmail.com>
2015-12-16 13:48:33 +0800

    Add activity_main.xml

---
+++ app/src/main/java/com/example/shana/androidlesson5_material_design/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.shana.androidlesson5_material_design;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by shana on 2015/12/16.
+ */
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
